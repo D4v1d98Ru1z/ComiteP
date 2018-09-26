74e3fc9405e835ddc7ba8cabaa6ec5042eec4732
D4v1d98Ru1z <davidl.ruizc@gmail.com>
2018-09-25 23:31:39 -0500

    UPDATE: Analytics implemented, fix the icon

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#gra-circular").circliful({
+        animation: 1,
+        animationStep: 5,
+        foregroundBorderWidth: 7,
+        backgroundBorderWidth: 7,
+        textSize: 28,
+        textStyle: 'font-size: 12px;',
+        textColor: '#666',
+        multiPercentage: 1,
+        percentages: [
+            {'percent': 80, 'color': '#3180B8', 'title': 'Ingreso de estudiantes' },
+            {'percent': 75, 'color': '#4ADBEA', 'title': 'Ingreso Directores' },
+            {'percent': 70, 'color': '#49EBA8', 'title': 'Ingreso de Jurados' },
+            {'percent': 50, 'color': '#FFCA35', 'title': 'Total visitas' }
+        ],
+        multiPercentageLegend: 1,
+        replacePercentageByText: '',
+        backgroundColor: '#eee',
+        icon: 'f0d0',
+        iconPosition: 'middle',
+        iconColor: '#273B4E'
+    });
+});
